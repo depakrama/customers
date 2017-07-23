60b3e25eff12796c54bcd1f329ddc4b6a49218ad
depakrama <depakrama@yahoo.com>
2017-07-23 15:47:52 +1000

    Update CreateCustomerTransformer.java

--- CreateCustomerTransformer.java
+++ CreateCustomerTransformer.java
@@ -1,41 +1,35 @@
 package au.com.customers.transformer;
-
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
-
 import au.com.customers.model.CustomerResponse;
 
+/**
+ * @author Depak Java Transformer Class to get the payload and generate JSON
+ *         Response.
+ *
+ */
+public class CreateCustomerTransformer extends AbstractMessageTransformer {
 
-
-
-public class CreateCustomerTransformer extends AbstractMessageTransformer  {
-
+	/*
+	 * (non-Javadoc) This Method gets the JSON Payload and builds the JSON
+	 * Response for POST Create Customer.
+	 */
 	@Override
-	public Object transformMessage(MuleMessage message, String outputEncoding)
-			throws TransformerException {
+	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
 
 		try {
 			String request = message.getPayloadAsString();
-			
-			System.out.println("request-----"+request);
+
+			System.out.println("request-----" + request);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		CustomerResponse response = new CustomerResponse();
 		response.setCustomerId("2");
 		return response;
 
-		
 	}
 
-
-	
-
-	}
-
-
-
-
+}
