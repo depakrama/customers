4875351c4b3c801ce12a7dc687bea561a4d04b59
depakrama <depakrama@yahoo.com>
2017-07-23 15:48:55 +1000

    Update DeleteCustomerTransformer.java

--- DeleteCustomerTransformer.java
+++ DeleteCustomerTransformer.java
@@ -1,47 +1,37 @@
 package au.com.customers.transformer;
 
 import java.util.Map;
-
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
-
 import au.com.customers.model.CustomerResponse;
 
+/**
+ * @author Depak
+ * Java Transformer Class for DELETE call for Customer
+ *
+ */
+public class DeleteCustomerTransformer extends AbstractMessageTransformer {
 
-
-
-public class DeleteCustomerTransformer extends AbstractMessageTransformer  {
-
+	/* (non-Javadoc)
+	 * This method extracts the customer Id and builds the response.
+	 */
 	@Override
-	public Object transformMessage(MuleMessage message, String outputEncoding)
-			throws TransformerException {
-		String cusId="";
+	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
+		String cusId = "";
 		try {
-		     
-		     Map<String, String> UriParams = message.getInboundProperty("http.uri.params");
-		      cusId=UriParams.get("customer");
-		     System.out.println("customer id: "+cusId);
-			
-			///System.out.println("request-----"+req.getCustomerId());
+
+			Map<String, String> UriParams = message.getInboundProperty("http.uri.params");
+			cusId = UriParams.get("customer");
+			System.out.println("customer id: " + cusId);
+
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		CustomerResponse response = new CustomerResponse();
-		//response.setCustomerId("Customer Deleted successfully");
-		
 		return response;
 
-		
 	}
 
-
-	
-
-	}
-
-
-
-
+}
