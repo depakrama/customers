681dda451b5cf6dc8b31b11858d5276e47651cc3
depakrama <depakrama@yahoo.com>
2017-07-23 15:51:50 +1000

    Update getCustomerTransformer.java

--- getCustomerTransformer.java
+++ getCustomerTransformer.java
@@ -1,34 +1,36 @@
 package au.com.customers.transformer;
 
 import java.util.Map;
-
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
-
 import au.com.customers.model.GetCustomerResponse;
 
+/**
+ * @author Depak Java Transformer Class for GET call for Get Customer based on
+ *         Customer Id.
+ *
+ */
+public class getCustomerTransformer extends AbstractMessageTransformer {
 
-
-
-public class getCustomerTransformer extends AbstractMessageTransformer  {
-
+	/*
+	 * (non-Javadoc) this method gets the Customer Id from the uri and builds
+	 * the response.
+	 */
 	@Override
-	public Object transformMessage(MuleMessage message, String outputEncoding)
-			throws TransformerException {
-		String cusId="";
+	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
+		String cusId = "";
 		try {
-		     
-		     Map<String, String> UriParams = message.getInboundProperty("http.uri.params");
-		      cusId=UriParams.get("customer");
-		     System.out.println("customer id: "+cusId);
-			
-			///System.out.println("request-----"+req.getCustomerId());
+
+			Map<String, String> UriParams = message.getInboundProperty("http.uri.params");
+			cusId = UriParams.get("customer");
+			System.out.println("customer id: " + cusId);
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		GetCustomerResponse response = new GetCustomerResponse();
 		response.setCustomerId(cusId);
 		response.setName("test");
@@ -37,14 +39,6 @@ public class getCustomerTransformer extends AbstractMessageTransformer  {
 		response.setPostalcode("2160");
 		return response;
 
-		
 	}
 
-
-	
-
-	}
-
-
-
-
+}
