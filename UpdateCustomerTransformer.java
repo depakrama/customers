c972dcb69e56fdffef2f76e612620de955f3b1a2
depakrama <depakrama@yahoo.com>
2017-07-23 15:50:20 +1000

    Update UpdateCustomerTransformer.java

--- UpdateCustomerTransformer.java
+++ UpdateCustomerTransformer.java
@@ -3,39 +3,33 @@ package au.com.customers.transformer;
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
-
 import au.com.customers.model.CustomerResponse;
 
+/**
+ * @author Depak
+ * Java Transformer Class for update customer
+ *
+ */
+public class UpdateCustomerTransformer extends AbstractMessageTransformer {
 
-
-
-public class UpdateCustomerTransformer extends AbstractMessageTransformer  {
-
+	/* (non-Javadoc)
+	 * this method extracts the customer Id from the URI and builds the response.
+	 */
 	@Override
-	public Object transformMessage(MuleMessage message, String outputEncoding)
-			throws TransformerException {
+	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
 
 		try {
 			String request = message.getPayloadAsString();
-			
-			System.out.println("request-----"+request);
+
+			System.out.println("request-----" + request);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		CustomerResponse response = new CustomerResponse();
 		response.setCustomerId("3");
 		return response;
 
-		
 	}
 
-
-	
-
-	}
-
-
-
-
+}
